76c4bb51883915f399861ca41b99ac31e3c88464
Cabe Bartz <cabebartz@fullsai.edu>
2012-07-04 22:05:30 -0700

    made changes to add item.html to work better with JS Added beginning function storeData to store submitted info into local storage

--- js/main.js
+++ js/main.js
@@ -25,15 +25,36 @@ window.addEventListener("DOMContentLoaded", function(){
 		}
 		selectLi.appendChild(createSelect);
 	}
+	//function to store data in local storage
+	function storeData(){
+		var id = Math.floor(Math.random() * 1000100001);
+		//store form fields in object
+		//object will contain arrat with form label and input value
+		var item = {};
+		item.category = ["Bill Category:", ElId('select').value];
+		item.billName = ["Bill Name:", ElId('billName').value];
+		item.accountNum = ["Account Number:", ElId('accountNum').value];
+		item.billAmount = ["Amount Due:", ElId('billAmount').value];
+		item.dueDate = ["Date Due:", ElId('dueDate').value];
+
+		//  item.billPaid = ["Is Bill Paid?", paidValue];
+
+		item.priority = ["Priority:", ElId('priority').value];
+		item.comments = ["comments:", ElId('comments').value];
+
+		//Save into local storage and convert to string(JSON.stringify)
+		localStorage.setItem(id, JSON.stringify(item));
+		alert("Bill Saved");
+	}
 	//variable defaults
 	var billCategory = ["Credit", "Rent", "Utilities", "Misc"];
+	makeCategory();
 	//Link and click events
 	/* var displayData = ElId('displayData');
 	displayData.addEventListener("click", getData);
 	var clearData = ElId('clearData');
 	clearData.addEventListener("click", clearData);
-	var saveData = ElId('submit');
-	save.addEventListener("click", storeData);
 	*/
-	makeCategory();
+	var saveData = ElId('submit');
+	saveData.addEventListener("click", storeData);
 });
