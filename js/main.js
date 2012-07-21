e1a5d40cf238b635049120db99761cc668e9fcf3
Cabe Bartz <cabebartz@fullsai.edu>
2012-07-21 16:56:26 -0700

    changed error messages to not be alerts due to errors removed bullets on li's

--- js/main.js
+++ js/main.js
@@ -215,32 +215,32 @@ window.addEventListener("DOMContentLoaded", function () {
 		getNotPaid.style.border = "1px solid grey";
 		//category validation
 		if(getCategory.value === "-- Pick A Category --"){
-			catError = alert("Please pick a category");
+			catError = "Please pick a category";
 			getCategory.style.border = "1px solid red";
 			messageArray.push(catError);
 		}
 		//bill Name validation
 		if (getName.value === ""){
-			nameError = alert("Please enter a bill name");
+			nameError = "Please enter a bill name";
 			getName.style.border = "1px solid red";
 			messageArray.push(nameError);
 		}
 		//bill amount validation
 		var dollarRe = /^\d+\.\d{2}$/;
 		if (!(dollarRe.exec(getAmount.value))){
-			amountError = alert("Please enter amount due in format XXX.XX");
+			amountError = "Please enter amount due in format XXX.XX";
 			getAmount.style.border = "1px solid red";
 			messageArray.push(amountError);
 		}
 		// date validation
 		if (getDate.value === ""){
-			dateError = alert("Please enter the due date");
+			dateError = "Please enter the due date";
 			getDate.style.border = "1px solid red";
 			messageArray.push(dateError);
 		}
 		//bill paid validation
 		if (!(paid.checked) && !(notPaid.checked)){
-			paidError = alert("Please choose a paid option");
+			paidError = "Please choose a paid option";
 			getPaid.style.border = "1px solid red";
 			getNotPaid.style.border = "1px solid red";
 			messageArray.push(paidError);
