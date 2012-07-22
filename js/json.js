adbd7ab7cd873e285a7062d5cc4ea83be9b3c628
Cabe Bartz <cabebartz@fullsai.edu>
2012-07-21 17:52:49 -0700

    no message

---
+++ js/json.js
@@ -0,0 +1,22 @@
+var json = {
+	"bill1":{
+		"category": ["Bill Category:", "Credit"],
+		"billName": ["Bill Name:", "Bank Of America"],
+		"accountNum": ["Account Number:", "12345"],
+		"billAmount": ["Amount Due:", "125.00"],
+		"dueDate": ["Date Due:", "2012-10-11"],
+		"billPaid": ["Is Bill Paid?", "Paid"],
+		"priority": ["Priority:", "5"],
+		"comments": ["comments:", "already paid"]
+	},
+	"bill2":{
+		"category": ["Bill Category:", "Rent"],
+		"billName": ["Bill Name:", "KPM"],
+		"accountNum": ["Account Number:", "1234589"],
+		"billAmount": ["Amount Due:", "1125.00"],
+		"dueDate": ["Date Due:", "2012-10-15"],
+		"billPaid": ["Is Bill Paid?", "Not Paid"],
+		"priority": ["Priority:", "9"],
+		"comments": ["comments:", "Pay Soon"]
+	}
+}
